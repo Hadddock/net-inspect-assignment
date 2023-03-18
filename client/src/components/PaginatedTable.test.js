276684c552f07239ba647dd2719a23267c9e8e0b
Haddock <jhadddock@protonmail.com>
2023-03-18 13:09:41 -0700

    Add mock data to PaginatedTable.test.js

--- client/src/components/PaginatedTable.test.js
+++ client/src/components/PaginatedTable.test.js
@@ -7,3 +7,59 @@ test("renders learn react link", () => {
   const linkElement = screen.getByText(/test/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+describe("Create Paginated Table", () => {
+  beforeEach(() => {
+    const data = [
+      {
+        classification: "Baby",
+        ctq: false,
+        isQualified: true,
+        jurisdiction: "Research and Development",
+        lastUpdatedBy: "tmccahey0",
+        lastUpdatedDate: "2017-11-24T03:43:02Z",
+        openPo: false,
+        partName: "Kanlam",
+        partNumber: "YOKWIA",
+        qplExpirationDate: "2019-04-08T14:14:17Z",
+        revision: "HRHD",
+        supplierCode: "36987-1760",
+        supplierName: "Aimbo",
+        toolDieSetNumber: "OSJJO4",
+      },
+      {
+        classification: "Garden",
+        ctq: false,
+        isQualified: true,
+        jurisdiction: "Training",
+        lastUpdatedBy: "sbarhams1",
+        lastUpdatedDate: "2018-08-06T14:26:51Z",
+        openPo: false,
+        partName: "Gembucket",
+        partNumber: "06WDD0",
+        qplExpirationDate: "2018-04-18T14:47:05Z",
+        revision: "V20W",
+        supplierCode: "64725-0221",
+        supplierName: "Kwimbee",
+        toolDieSetNumber: "RCS6FK",
+      },
+      {
+        classification: "Movies",
+        ctq: false,
+        isQualified: true,
+        jurisdiction: "Human Resources",
+        lastUpdatedBy: "neason2",
+        lastUpdatedDate: "2017-12-06T23:53:23Z",
+        openPo: true,
+        partName: "Toughjoyfax",
+        partNumber: "B8IS2O",
+        qplExpirationDate: "2019-10-31T20:08:00Z",
+        revision: "60P4",
+        supplierCode: "43857-0140",
+        supplierName: "Tambee",
+        toolDieSetNumber: "LL0N2K",
+      },
+    ];
+    <PaginatedTable data={data} />;
+  });
+});

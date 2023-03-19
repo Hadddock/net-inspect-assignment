a334a2ed9fc22e92c8d2c1c0ab613a4014604399
Haddock <jhadddock@protonmail.com>
2023-03-18 21:34:36 -0700

    Add test do not execute search on blank query

--- client/src/components/PaginatedTable.test.js
+++ client/src/components/PaginatedTable.test.js
@@ -289,3 +289,14 @@ test("search case insensitive", async () => {
   //number of data rows matching search query (2) + column header row (1)
   expect(rows.length).toBe(3);
 });
+
+test("do not execute search on blank query", async () => {
+  render(<PaginatedTable data={data} />);
+  const searchBar = screen.getByRole("textbox");
+  const searchButton = screen.getByAltText("search button");
+  act(() => userEvent.type(searchBar, ""));
+  act(() => userEvent.click(searchButton));
+  const rows = screen.getAllByRole("row");
+  //number of data rows total + column header row (1)
+  expect(rows.length).toBe(5);
+});

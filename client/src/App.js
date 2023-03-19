73ac8094631243cf40ac2c5af45f1f31cd1d413b
Haddock <jhadddock@protonmail.com>
2023-03-19 15:22:49 -0700

    Add column name remapings prop  to App

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,22 @@ function App() {
       {data ? (
         <PaginatedTable
           data={data}
+          exclude={["qplExpirationDate"]}
+          columnHeaderNameMappings={{
+            partNumber: "Part Number",
+            revision: "Part Revision",
+            partName: "Part Name",
+            toolDieSetNumber: "Tool / Die Set Number",
+            isQualified: "QPL",
+            openPo: "Open PO",
+            jurisdiction: "Part Jurisdiction",
+            classification: "Part Classification",
+            supplierName: "Supplier Company Name",
+            supplierCode: "Supplier Company Code",
+            ctq: "CTQ",
+            lastUpdatedBy: "QPL Last Updated By",
+            lastUpdatedDate: "QPL Last Updated Date",
+          }}
           columnHeaderOrder={[
             "partNumber",
             "revision",
